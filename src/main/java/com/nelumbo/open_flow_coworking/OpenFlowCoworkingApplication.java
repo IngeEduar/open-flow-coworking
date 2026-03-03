aa6e82b7383037e7763e214975a86a1b76f45790
Eduar Xavier Avendaño <xavieravendano9@gmail.com>
2026-03-02 19:34:41 -0500

    Initial commit

---
+++ src/main/java/com/nelumbo/open_flow_coworking/OpenFlowCoworkingApplication.java
@@ -0,0 +1,13 @@
+package com.nelumbo.open_flow_coworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFlowCoworkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFlowCoworkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelumbo/open_flow_coworking/OpenFlowCoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelumbo.open_flow_coworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFlowCoworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
